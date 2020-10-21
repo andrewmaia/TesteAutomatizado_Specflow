ded10fbfa30cd266fa4e525290385b6816d39365
Andrew Santos Maia <andrew.maia@talktelecom.com.br>
2020-10-21 11:21:23 -0300

    Commit Inicial

---
+++ CalculadoraProject/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraProject
+{
+    public class Calculadora
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
---
+++ Specflow_Test/CalculadoraSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using CalculadoraProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Specflow_Test
+{
+    [Binding]
+    public class CalculadoraSteps
+    {
+        private int result;
+        Calculadora calculadora = new Calculadora();
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculadora.FirstNumber = number;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            calculadora.SecondNumber = number;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculadora.Add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
